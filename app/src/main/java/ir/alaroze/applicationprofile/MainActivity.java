0c1d0204c4a7d944bee50635748ee7ebfb38cef1
erfan-ghasemi <erfanghasemiioogg@gmail.com>
2024-05-27 14:36:23 +0330

    add listener on MainActivity

--- app/src/main/java/ir/alaroze/applicationprofile/MainActivity.java
+++ app/src/main/java/ir/alaroze/applicationprofile/MainActivity.java
@@ -1,6 +1,13 @@
 package ir.alaroze.applicationprofile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +26,58 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnEditProfile = findViewById(R.id.btn_main_editProfile);
+        Button ViewWebsite = findViewById(R.id.btn_main_viewWebsite);
+        TextView tvFullname = findViewById(R.id.tv_main_fullname);
+
+        CheckBox cbAndroidDevelopment = findViewById(R.id.cb_main_androidDevelopment);
+        CheckBox cbUiDesign = findViewById(R.id.cb_main_uiDesign);
+        CheckBox cbDeepLearning = findViewById(R.id.cb_main_deepLearning);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup_main_city);
+
+        Button btnSaveInformation = findViewById(R.id.btn_main_saveInformation);
+
+        cbAndroidDevelopment.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                Toast.makeText(this, "android Development is checked", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "android Development isn't checked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        cbUiDesign.setOnCheckedChangeListener(((buttonView, isChecked) ->{
+            if (isChecked){
+                Toast.makeText(this, "ui design is checked", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "ui design isn't checked", Toast.LENGTH_SHORT).show();
+
+            }
+        }));
+
+        cbDeepLearning.setOnCheckedChangeListener(((buttonView, isChecked) ->{
+            if (isChecked){
+                Toast.makeText(this, "deep learning is checked", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "deep learning isn't checked", Toast.LENGTH_SHORT).show();
+
+            }
+        }));
+
+
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.radioBtn_main_tehran)
+                Toast.makeText(this, "tehran is checked.", Toast.LENGTH_SHORT).show();
+            else if (checkedId == R.id.radioBtn_main_gilan)
+                Toast.makeText(this, "gilan is checked.", Toast.LENGTH_SHORT).show();
+            else if (checkedId == R.id.radioBtn_main_alborz)
+                Toast.makeText(this, "alborz is checked.", Toast.LENGTH_SHORT).show();
+        });
+
+        btnSaveInformation.setOnClickListener(v -> {
+            Toast.makeText(this, "user information are saved.", Toast.LENGTH_SHORT).show();
+
         });
     }
 }
